57d01cc0c07e9e272a2cc9961c92ca02877c4270
georgabase <swabston@gmail.com>
2018-04-03 16:06:42 -0400

    initial commit

---
+++ Register.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Web.UI;
+using EWallet;
+using System.Data;
+
+public partial class Account_Register : Page
+{
+    protected void CreateUser_Click(object sender, EventArgs e)
+    {
+        DataBaseHandler db = new DataBaseHandler();
+        db.AddUser(FirstName.Text,LastName.Text,Email.Text,Phone.Text,UserName.Text, Password.Text);
+        DataSet ds = new DataSet();
+        ds = db.CheckUser(Email.Text, Password.Text);
+        if (ds != null)
+        {
+            Session["UserInfo"] = ds;
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
