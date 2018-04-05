17fd577719c8aace9972691d7003400a32a52135
kunver <robertriemens1@gmail.com>
2018-04-05 00:53:16 -0400

    My start
    Added my parts so far

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <%--    &nbsp;<div class="jumbotron">
+       <%-- <h1>EWALLET</h1>
+        <%--<p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>--%>
+        <%--<p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+  </div>
+    
+    &nbsp;--%>
+            
+        <div class="jumbotron">
+        <ul>
+            <li style="width:800px"><asp:Button ID="BtnTransfer"   Font-Bold="True" Font-Size="XX-Large" style=" font-size:larger; background-image:url('image/transfer.png'); background-size:cover; cursor:hand;" Width="200px" Height="200px" runat="server" Text="Transfer " OnClick="BtnTransfer_Click" /></li>
+            <li style="width:800px"><asp:Button ID="BtnDeposit" Font-Bold="True" Font-Size="XX-Large" style=" font-size:larger; background-image:url('image/deposit.png'); background-size:cover; cursor:hand;" Width="200px" Height="200px"  runat="server" Text="Deposit " OnClick="BtnDeposit_Click" /></li>
+            <li style="width:800px"><asp:Button ID="BtnWithdraw" Font-Bold="True" Font-Size="XX-Large" style=" font-size:larger; background-image:url('image/withdraw.png'); background-size:cover; cursor:hand;" Width="200px" Height="200px"  runat="server" Text="Withdraw" OnClick="BtnWithdraw_Click" /></li>
+            <li style="width:800px"><asp:Button ID="BtnHistory" Font-Bold="True" Font-Size="XX-Large" style=" font-size:larger; background-image:url('image/history.png'); background-size:cover; cursor:hand;" Width="200px" Height="200px"  runat="server" Text="History" OnClick="BtnWithdraw_Click" /></li>
+  
+            </ul>
+            <center>
+            <asp:Label ID="msg" ForeColor="Red" runat="server" />
+                </center>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        BtnHistory.Visible = false;
+
+
+            if(Session["UserInfo"] !=null)
+            { 
+                DataSet ds = new DataSet();
+                ds = Session["UserInfo"] as DataSet;
+                if(ds.Tables[0].Rows.Count>0)
+                { 
+                Session["Userid"] = ds.Tables[0].Rows[0]["CustomerID"];
+                Control c = this.Master.FindControl("login");// "masterDiv"= the Id of the div.
+                c.Visible = false;
+                c = this.Master.FindControl("Register");// "masterDiv"= the Id of the div.
+                c.Visible = false;
+                c = this.Master.FindControl("LogOut");// "masterDiv"= the Id of the div.
+                c.Visible = true;
+                Label mpLabel = (Label)Master.FindControl("welcome");
+                mpLabel.Text= "Welcome "+ ds.Tables[0].Rows[0]["firstname"];
+                mpLabel.Visible = true;
+                ViewState["Flags"] = 1;
+                if (Convert.ToInt32(ds.Tables[0].Rows[0]["RollID"]) == 1)
+                {
+                    BtnHistory.Visible = true;
+                }
+
+                }
+
+            }
+        
+    }
+
+    protected void BtnTransfer_Click(object sender, EventArgs e)
+    {
+        if (ViewState["Flags"]!= null)
+            Response.Redirect("Transfer.aspx");
+        else
+            msg.Text = "Please login to use this feature";
+    }
+
+    protected void BtnDeposit_Click(object sender, EventArgs e)
+    {
+        if (ViewState["Flags"].ToString() == "1")
+            Response.Redirect("DepositForm.aspx");
+        else
+            msg.Text = "Please login to use this feature";
+    }
+
+    protected void BtnWithdraw_Click(object sender, EventArgs e)
+    {
+        if (ViewState["Flags"].ToString() == "1")
+            Response.Redirect("BankDetails.aspx");
+        else
+            msg.Text = "Please login to use this feature";
+    }
+    protected void BtnHistory_Click(object sender, EventArgs e)
+    {
+        if (ViewState["Flags"].ToString() == "1")
+            Response.Redirect("TransactionHistory.aspx");
+        else
+            msg.Text = "Please login to use this feature";
+    }
+}
