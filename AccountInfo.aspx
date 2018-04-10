379047fe20c8fc72924e812e08d7ab7839e6252b
ROBINLEMUEL13 <38051821+ROBINLEMUEL13@users.noreply.github.com>
2018-04-10 17:35:59 -0400

    Add files via upload

---
+++ AccountInfo.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="AccountInfo.aspx.cs" Inherits="AccountInfo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <center><div>
+    <h1>User Details</h1>
+    <table>
+        <tr><td>Customer ID</td><td> <asp:Label ID="LabelCustomerId" runat="server" Text="Customer Id"></asp:Label></td></tr>
+        <tr><td>First Name</td><td><asp:Label ID="LabelFirstName" runat="server" Text="First NAme"></asp:Label></td></tr>
+        <tr><td>Last Name</td><td><asp:Label ID="LabelLastName" runat="server" Text="Last name"></asp:Label></td></tr>
+        <tr><td>Email</td><td><asp:Label ID="LabelEmail" runat="server" Text="Email"></asp:Label></td></tr>
+        <tr><td>Account No</td><td><asp:Label ID="LabelAccountNo" runat="server" Text="Account No"></asp:Label></td></tr>
+        <tr><td>Phone No</td><td><asp:Label ID="LabelPhoneNo" runat="server" Text="Phone No"></asp:Label></td></tr>
+        <tr><td></td><td><asp:Label ID="LabelRollID" runat="server" Text="Roll ID"></asp:Label></td></tr>
+    </table>
+    <br />
+        <asp:Label ID="LabelOutput" runat="server" ForeColor="Red" Text=""></asp:Label>
+        <br />
+    <asp:Button ID="BtnTransactionHistory" Class="button" runat="server" Text="Transaction Report" OnClick="BtnTransactionHistory_Click" />
+    <br /><br /><br />
+        <div id="test" runat="server" visible="false">
+    <asp:Label ID="Label1" runat="server" Text="Enter a Customer Id"></asp:Label>
+    <asp:TextBox ID="TextBoxCId" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" Class="button" runat="server" Text="Observe a Customer" OnClick="Button1_Click1" />
+            <asp:Button ID="Button2" Class="button" runat="server" Text="Edit Customer Info" OnClick="Check" />
+        </div>
+        </div></center>
+</asp:Content>
+
---
+++ AccountInfo.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AccountInfo : System.Web.UI.Page
+{
+    string Flag,CustID;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            if (Session["UserInfo"] != null)
+            {
+                DataSet ds = new DataSet();
+                ds = Session["UserInfo"] as DataSet;
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    Session["Userid"] = ds.Tables[0].Rows[0]["CustomerID"];
+                    Control c = this.Master.FindControl("login");// "masterDiv"= the Id of the div.
+                    c.Visible = false;
+                    c = this.Master.FindControl("Register");// "masterDiv"= the Id of the div.
+                    c.Visible = false;
+                    c = this.Master.FindControl("LogOut");// "masterDiv"= the Id of the div.
+                    c.Visible = true;
+                    Label mpLabel = (Label)Master.FindControl("welcome");
+                    mpLabel.Text = "Welcome " + ds.Tables[0].Rows[0]["firstname"];
+                    mpLabel.Visible = true;
+                    ViewState["Flag"] = ds.Tables[0].Rows[0]["rollid"].ToString();
+                    ViewState["CustID"] = ds.Tables[0].Rows[0]["CustomerID"].ToString();
+                    LabelCustomerId.Text = ds.Tables[0].Rows[0]["CustomerID"].ToString();
+                    LabelFirstName.Text = ds.Tables[0].Rows[0]["FirstName"].ToString();
+                    LabelLastName.Text = ds.Tables[0].Rows[0]["LastName"].ToString();
+                    LabelEmail.Text = ds.Tables[0].Rows[0]["Email"].ToString();
+                    LabelAccountNo.Text = ds.Tables[0].Rows[0]["AccountNo"].ToString();
+                    LabelPhoneNo.Text = ds.Tables[0].Rows[0]["Phone"].ToString();
+                    LabelRollID.Text = "";
+                    if (ds.Tables[0].Rows[0]["rollid"].ToString() == "1")
+                        test.Visible = true;
+                }
+                else
+                    Response.Redirect("login.aspx");
+
+            }
+            else
+                Response.Redirect("login.aspx");
+    }
+   
+
+    protected void BtnTransactionHistory_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Report.aspx?Flag="+ ViewState["Flag"] + " &CustID="+ ViewState["CustID"]);
+    }
+    DataBaseHandler cls = new DataBaseHandler();
+    DataSet ds = new DataSet();
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        if (TextBoxCId.Text.Equals(""))
+        {
+            //LabelOutput.Text = "Enter a customer Id";
+        }
+        else
+        {
+            int result;
+            if(int.TryParse(TextBoxCId.Text,out  result))
+            {
+                ds = cls.checkUserId(Convert.ToInt32(TextBoxCId.Text));
+            }
+          
+            if(ds.Tables[0].Rows.Count == 0)
+            {
+                LabelOutput.Text = "Enter a Valid customer Id";
+            }
+            else
+            {
+                Response.Redirect("Report.aspx?Flag=2 &CustID=" +result);
+            }
+        }
+    }
+
+    protected void Check(object sender, EventArgs e)
+    {
+        Response.Redirect("UpdateCustomer.aspx");
+    }
+}
